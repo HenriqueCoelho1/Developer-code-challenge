40a305779aab4efc806224fb00b171fafcdeab2f
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-09-29 23:33:02 -0300

    Routing.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-const pool = require('./db')
+const userRoutes = require('./routes/user')
 const { readdirSync } = require('fs')
 require('dotenv').config()
 const app = express();
@@ -11,79 +11,10 @@ app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
 
-app.post("/api/user", async (req, res) => {
-    try {
-        const { name, email, cpf } = req.body
-        const createPerson = await
-            pool.query("INSERT INTO person (name, email, cpf) VALUES($1, $2, $3) RETURNING *",
-                [name, email, cpf])
-        res.json(createPerson.rows[0])
-    } catch (err) {
-        console.log("CREATE ERROR", err.message)
-
-    }
-
-})
-
-app.get("/api/user/:id", async (req, res) => {
-    try {
-        const { id } = req.params
-        const listOne = await pool.query("SELECT * FROM person WHERE id = $1", [id])
-
-        res.json(listOne.rows[0])
-
-
-    } catch (err) {
-        console.log("CREATE ERROR", err.message)
-
-    }
-})
-
-app.get("/api/user", async (req, res) => {
-    try {
-        const listAll = await pool.query("SELECT * FROM person")
-
-        res.json(listAll)
-
-
-    } catch (err) {
-        console.log("CREATE ERROR", err.message)
-
-    }
-})
-
-app.put("/api/user/:id", async (req, res) => {
-    try {
-        const { id } = req.params
-        const { name, email } = req.body
-        const updateOne =
-            await pool.query("UPDATE person SET name = $1, email = $2 WHERE id = $3", [name, email, id])
-
-        res.json("User updated!")
-
-    } catch (err) {
-        console.log("UPDATE ERROR", err.message)
-
-    }
-})
-
-
-app.delete("/api/user/:id", async (req, res) => {
-    try {
-        const { id } = req.params
-        const deleteOne = await pool.query("DELETE FROM person WHERE id = $1", [id])
-
-        res.json("This person was deleted!")
-
-    } catch (error) {
-        console.log("REMOVE ERROR", err.message)
-
-    }
-})
 
 
 //routes
-// readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)))
+app.use('/api', userRoutes)
 
 const PORT = process.env.PORT || 5000
 
